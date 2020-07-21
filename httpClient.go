eb9d3fa8a7219257dec563f747822f9643485464
weizhouBlue <weizhou.lan@daocloud.io>
2020-07-21 21:28:35 +0800

    support tls and unix

--- httpClient.go
+++ httpClient.go
@@ -1,5 +1,6 @@
 package golib_httpUtility
 import (
+	"net"
     "net/http"
     "context"
     "strings"
@@ -7,6 +8,9 @@ import (
     "io"
     "time"
     "io/ioutil"
+
+    "crypto/x509"
+    "crypto/tls"
 )
 
 type HttpMethod string
@@ -20,7 +24,14 @@ const (
 
 )
 
-func HttpClient( severUrl string  , method HttpMethod , requestHeader map[string][]string, requestBody string , timeout int ) (  returnCode int , reponseBody string , reponseHeader map[string][]string,  err error ){
+type TlsConf struct {
+	IgnoreServerCa bool
+	CaPath string
+	CertPath string
+	KeyPath string
+}
+
+func HttpClient( severUrl string  , method HttpMethod , requestHeader map[string][]string, requestBody string , timeout int , unixSkPath string , tlsConf *TlsConf ) (  returnCode int , reponseBody string , reponseHeader map[string][]string,  err error ){
 
 	var ctx context.Context
 	//var cancel context.CancelFunc
@@ -29,12 +40,61 @@ func HttpClient( severUrl string  , method HttpMethod , requestHeader map[string
 	var er error
 	var msg io.Reader
 	var response *http.Response
-	var httpClient = &http.Client{}
 
 	err=nil
 	returnCode=0
 	reponseBody=""
 	reponseHeader=nil
+
+	if timeout==0 {
+		timeout=60
+	}
+
+
+	// https://godoc.org/net/http#Transport
+	tr:=&http.Transport{}
+
+	if tlsConf!=nil {
+		// https://godoc.org/crypto/tls#Config
+	    tr.TLSClientConfig=&tls.Config{ InsecureSkipVerify: tlsConf.IgnoreServerCa }
+		if len(tlsConf.CaPath)!=0 {
+		    pool := x509.NewCertPool()
+		    caCrt, e1 := ioutil.ReadFile(tlsConf.CaPath)
+		    if e1 != nil {
+				err=fmt.Errorf(  "ReadFile err:", e1  )
+		        return
+		    }
+		    pool.AppendCertsFromPEM(caCrt)
+		    tr.TLSClientConfig.RootCAs=pool
+		}
+
+		if len(tlsConf.CertPath)!=0 && len(tlsConf.KeyPath)!=0  {
+		    cliCrt, e2 := tls.LoadX509KeyPair( tlsConf.CertPath , tlsConf.KeyPath )
+		    if e2 != nil {
+				err=fmt.Errorf(  "Loadx509keypair err:", e2  )
+		        return
+		    }
+		    tr.TLSClientConfig.Certificates=[]tls.Certificate{cliCrt}
+		}
+	}
+
+	if len(unixSkPath)!=0 {
+		 tr.Dial = func( _ , _ string) (net.Conn, error) {
+		 	return net.Dial("unix", unixSkPath  )
+		 	// return	( &net.Dialer{
+				//         KeepAlive: 30 * time.Second,
+				//         DualStack: true,
+				//     }).Dial("unix", unixSkPath  )
+		 }
+
+	}
+
+	//tr.ForceAttemptHTTP2=true
+	// maximum amount of time an idle (keep-alive) connection will remain idle before closing itself
+	//tr.IdleConnTimeout=3 0 * time.Second
+
+	// https://godoc.org/net/http#Client
+	httpClient := &http.Client{Transport: tr}
 
 	if len(severUrl) == 0 {
 		err=fmt.Errorf( "empty url " )
@@ -42,11 +102,7 @@ func HttpClient( severUrl string  , method HttpMethod , requestHeader map[string
 	}
 
 
-	if timeout > 0 {
-		ctx, _ = context.WithTimeout(context.Background(), time.Duration(timeout) * time.Second )
-	}else{
-		ctx, _ = context.WithCancel(context.Background() )
-	}
+	ctx, _ = context.WithTimeout(context.Background(), time.Duration(timeout) * time.Second )
 
 	if len(requestBody)>0{
 		msg=strings.NewReader(requestBody)
@@ -54,6 +110,7 @@ func HttpClient( severUrl string  , method HttpMethod , requestHeader map[string
 		msg=nil
 	}
 
+	// generate a request : https://godoc.org/net/http#Request
 	request, er=http.NewRequestWithContext( ctx , string(method) , severUrl , msg )
 	if er!=nil {
 		err=fmt.Errorf( "%v", er )
@@ -64,6 +121,7 @@ func HttpClient( severUrl string  , method HttpMethod , requestHeader map[string
 		request.Header=requestHeader
 	}
 
+	// send request 
 	response , er=httpClient.Do(request)
 	if er!=nil{
 		err=fmt.Errorf( "%v", er )
@@ -89,14 +147,6 @@ func HttpClient( severUrl string  , method HttpMethod , requestHeader map[string
 
 	return
 }
-
-
-
-
-
-
-
-
 
 
 
--- my_test.go
+++ my_test.go
@@ -10,15 +10,16 @@ import (
 func Test_http(t *testing.T){
 
 
-    severUrl:="http://10.6.185.160:6081/parcel/v1/manager/ping"
+    severUrl:="http://10.6.185.10:11160/parcel/v1/manager/ping"
     method:=http.MethodGet
     requestHeader:= map[string][]string {
         "testHeader":  {"12345"} ,
     }
     requestBody:=""
     timeout:=0
+    unixSkPath:=""
 
-    returnCode , reponseBody , reponseHeader ,  err := http.HttpClient( severUrl   , method ,requestHeader , requestBody  , timeout  ) 
+    returnCode , reponseBody , reponseHeader ,  err := http.HttpClient( severUrl   , method ,requestHeader , requestBody  , timeout ,unixSkPath , nil ) 
     if err!=nil {
         fmt.Println("err: " , err )
         t.FailNow()
@@ -29,4 +30,64 @@ func Test_http(t *testing.T){
 
 
 }
+
+
+func Test_https(t *testing.T){
+
+    severUrl:="https://10.6.185.10:6443/"
+    method:=http.MethodGet
+    requestHeader:= map[string][]string {}
+    requestBody:=""
+    timeout:=0
+    unixSkPath:=""
+    tlsConf:=  &http.TlsConf{
+        IgnoreServerCa : false ,
+        CaPath : "/etc/kubernetes/pki/ca.crt" ,
+        CertPath : "/etc/kubernetes/pki/apiserver-kubelet-client.crt" ,
+        KeyPath : "/etc/kubernetes/pki/apiserver-kubelet-client.key",
+    }
+
+    returnCode , reponseBody , reponseHeader ,  err := http.HttpClient( severUrl   , method ,requestHeader , requestBody  , timeout ,unixSkPath , tlsConf ) 
+    if err!=nil {
+        fmt.Println("err: " , err )
+        t.FailNow()
+    }
+    fmt.Println("return code: " , returnCode)
+    fmt.Println("response header: ", reponseHeader)
+    fmt.Println("reponse body: " , reponseBody)
+
+}
+
+
+// can use : socat -d -d UNIX-LISTEN:/tmp/testunix.socket  tcp:10.6.185.10:6443
+func Test_unix(t *testing.T){
+
+    severUrl:="https://10.6.185.10:6443/"
+    method:=http.MethodGet
+    requestHeader:= map[string][]string {}
+    requestBody:=""
+    timeout:=0
+    unixSkPath:="/tmp/test.socket"
+    tlsConf:=  &http.TlsConf{
+        IgnoreServerCa : true ,
+        CaPath : "" ,
+        CertPath : "" ,
+        KeyPath : "",
+    }
+
+    returnCode , reponseBody , reponseHeader ,  err := http.HttpClient( severUrl   , method ,requestHeader , requestBody  , timeout ,unixSkPath , tlsConf ) 
+    if err!=nil {
+        fmt.Println("err: " , err )
+        t.FailNow()
+    }
+    fmt.Println("return code: " , returnCode)
+    fmt.Println("response header: ", reponseHeader)
+    fmt.Println("reponse body: " , reponseBody)
+
+
+}
+
+
+
+
 
